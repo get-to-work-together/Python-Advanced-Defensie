8928c31b339cd6a20c7313112aa872a334f7386e
Peter <email@peteranema.nl>
2024-01-29 15:02:02 +0100

    Update

--- Training/demo beautifulsoup.py
+++ Training/demo beautifulsoup.py
@@ -7,37 +7,47 @@ from bs4 import BeautifulSoup
 
 filename = 'config.json'
 with open(filename) as f:
-    config = json.load(f)
+    config_list = json.load(f)
 
+for config in config_list:
+    name = config['name']
+    url = config['url']
+    items_selector = config['items_selector']
+    string_selector = config['string_selector']
+    regex = config['regex']
+    current_version = tuple(config['current_version']) if config['current_version'] else None
 
-r = requests.get(url)
+    r = requests.get(url)
 
-if r.status_code != 200:
-    print(f'Could not get open {url}')
-    sys.exit()
+    if r.status_code != 200:
+        print(f'Could not get open {url}')
+        sys.exit()
 
-soup = BeautifulSoup(r.text, features="html.parser")
+    soup = BeautifulSoup(r.text, features="html.parser")
 
-items = soup.select(items_selector)
+    items = soup.select(items_selector)
 
-for item in items:
-    if string_selector:
-        title = item[string_selector]
-    else:
-        title = item.text
-
-    match = re.match(regex, title)
-
-    if match:
-        name, major, minor, rev, sub = match.groups()
-
-        if current_version:
-            if len(current_version) == 1 and (major, ) == current_version:
-                print(name, major, minor, rev, sub)
-            elif len(current_version) == 2 and (major, minor) == current_version:
-                print(name, major, minor, rev, sub)
-            elif len(current_version) == 3 and (major, minor, rev) == current_version:
-                print(name, major, minor, rev, sub)
+    for item in items:
+        if string_selector:
+            title = item[string_selector]
         else:
-            print(name, major, minor, rev, sub)
+            title = item.text
+
+        match = re.match(regex, title)
+
+        if match:
+            name, major, minor, rev, sub = match.groups()
+            major = int(major) if major else None
+            minor = int(minor) if minor else None
+            rev = int(rev) if rev else None
+
+            if current_version:
+                if len(current_version) == 1 and (major, ) == current_version:
+                    print(name, major, minor, rev, sub)
+                elif len(current_version) == 2 and (major, minor) == current_version:
+                    print(name, major, minor, rev, sub)
+                elif len(current_version) == 3 and (major, minor, rev) == current_version:
+                    print(name, major, minor, rev, sub)
+            else:
+                print(name, major, minor, rev, sub)
 
