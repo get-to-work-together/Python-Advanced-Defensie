4d273320fbb4670dba6599bf9c968f6b00c7fc35
Peter <email@peteranema.nl>
2024-01-29 14:14:07 +0100

    Update demo beautifulsoup.py

--- Training/demo beautifulsoup.py
+++ Training/demo beautifulsoup.py
@@ -10,6 +10,18 @@ items_selector = 'li.listitem a'
 string_selector = 'title'
 regex = r'^(\w+) (\d+)\.(\d+)\.(\d+)-?(.*)$'
 
+# Kibana Guides
+url = 'https://www.elastic.co/guide/en/kibana/index.html'
+items_selector = 'li.listitem a'
+string_selector = None
+regex = r'^(.*): *(\d+)\.(\d+)\.?(\d*)[- ]?(.*)$'
+
+# Confluence Release Notes
+url = 'https://confluence.atlassian.com/doc/confluence-release-notes-327.html'
+items_selector = 'div.content-section ul li a'
+string_selector = None
+regex = r'^(Confluence) (\d+)\.(\d+)\.?(\d*) ?(.*)$'
+
 
 r = requests.get(url)
 
@@ -22,10 +34,15 @@ soup = BeautifulSoup(r.text, features="html.parser")
 items = soup.select(items_selector)
 
 for item in items:
-    title = item[string_selector]
+    if string_selector:
+        title = item[string_selector]
+    else:
+        title = item.text
 
     match = re.match(regex, title)
-    name, major, minor, rev, sub = match.groups()
 
-    print(name, major, minor, rev, sub)
+    if match:
+        name, major, minor, rev, sub = match.groups()
+
+        print(name, major, minor, rev, sub)
 
