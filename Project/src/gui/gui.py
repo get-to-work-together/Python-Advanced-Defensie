b125a93f8afeb3569acff5b7a69e460baf0b0723
Peter <email@peteranema.nl>
2024-01-30 13:33:12 +0100

    Update gui.py

--- Project/src/gui/gui.py
+++ Project/src/gui/gui.py
@@ -1,16 +1,47 @@
-import tkinter as tk
+# import tkinter as tk
+import customtkinter as ctk
 
-class App(tk.Frame):
+class ListFrame(ctk.CTkFrame):
 
     def __init__(self, master=None):
         super().__init__(master)
+
+
+class DetailFrame(ctk.CTkFrame):
+
+    def __init__(self, master=None):
+        super().__init__(master)
+
+
+class App(ctk.CTkFrame):
+
+    def __init__(self, master=None):
+        super().__init__(master)
+
+        self.detail = DetailFrame(self)
+        self.detail.grid(column=0, row=0, sticky='news')
+
+        self.list = ListFrame(self)
+        self.list.grid(column=0, row=0, sticky='news')
+
+        self.show_list()
+
+    def show_detail(self):
+        self.detail.tkraise()
+
+    def show_list(self):
+        self.list.tkraise()
+
+
 
 
 
 
 
 def main():
-    root = tk.Tk()
+    root = ctk.CTk()
+    ctk.set_appearance_mode('dark')
+    ctk.set_default_color_theme('dark-blue')
     root.geometry('600x500+300+300')  # width x height + x_offset + y_offset
     root.resizable(width = False, height = False)
     root.title('My GUI')
