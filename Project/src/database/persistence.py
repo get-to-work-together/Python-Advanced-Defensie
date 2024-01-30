201ba165a57be77f7a9957b1edd88d5f776d0be8
Peter <email@peteranema.nl>
2024-01-30 15:09:52 +0100

    Update

---
+++ Project/src/database/persistence.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+dbname = 'applications.db'
+
+def save_application_record(name, version):
+    sql = "INSERT INTO applications (name, version) VALUES (?, ?)"
+
+    connection = sqlite3.connect(dbname)
+
+    connection.execute(sql, (name, version))
+
+    connection.commit()
+
+    connection.close()
+
+
+# -----
+
+if __name__ ==  '__main__':
+    save_application_record('XXX', 'Version 3')
